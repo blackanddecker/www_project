62b5a5731c2da19ec923df6ba1e7980b4c3e07b8
Katerina Kyriakou <katerina.kir12@gmail.com>
2020-01-04 13:28:15 +0200

    Fixed getFloat in all emissions servlet

--- src/AllEmissionsServlet.java
+++ src/AllEmissionsServlet.java
@@ -19,19 +19,18 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/AllEmissionsServlet")
 public class AllEmissionsServlet extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
-		
-		/* Check which type was selected */
+		PrintWriter out = response.getWriter();		
 		String username = request.getParameter("username");
-		
 		if(username != null) {
 			boolean selectFoodEmissionsStatus = selectFoodEmissions(username);
             boolean selectTransportEmissionsStatus = selectTransportEmissions(username);
 
 			if(selectFoodEmissionsStatus && selectTransportEmissionsStatus) {
-				// request.setAttribute("carbon", carbonQuantity);
 				RequestDispatcher rd = request.getRequestDispatcher("allEmissions.jsp");
 				rd.forward(request, response);
 			} else {
@@ -42,32 +41,7 @@ public class AllEmissionsServlet extends HttpServlet {
 		}
 		
 	}
-	
-	public float calculateCarbonInFood(String foodType, float quantity) {
-		/*Select query from Table CarbonByFood*/
-		ConnectionDetails connDetails = new ConnectionDetails();
-		Connection conn = connDetails.getConnection();
-		String sql = "SELECT * FROM CarbonByFood WHERE foodType=?";
-		float carbonPerKg, carbonPerQuantity = 0;
-		try {
-			PreparedStatement preparedStatement = conn.prepareStatement(sql);
-			preparedStatement.setString(1, foodType);
-			ResultSet resultSet = preparedStatement.executeQuery();
-			while(resultSet.next()) {
-				carbonPerKg = resultSet.getFloat("carbonPerKg");		
-				/*CALCULATE CARBON PER QUANTITY*/
-				carbonPerQuantity = carbonPerKg * quantity;	
-			}		
-			preparedStatement.close();
-			conn.close();
-		} catch(SQLException ex) {
-			ex.printStackTrace();
-		} catch(Exception ex) {
-			ex.printStackTrace();
-		}
-		return carbonPerQuantity;
-	}
-	
+	/* NOTE: REMEMBER TO HANDLE EMPTY TABLES ACCORDINGLY*/
 	public boolean selectFoodEmissions(String username) {
 		boolean status = false;
 		
@@ -77,14 +51,14 @@ public class AllEmissionsServlet extends HttpServlet {
 		try {
 			PreparedStatement preparedStatement = conn.prepareStatement(sql);
 			preparedStatement.setString(1, username);
-			ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next())
+			ResultSet rs = preparedStatement.executeQuery();
+            while (rs.next())
                 {
                     status = true;
 
                     String foodType = rs.getString("foodType");
-                    float foodQuantity = rs.getString("foodQuantity");
-                    float carbonQuantity = rs.getDate("carbonQuantity");
+                    float foodQuantity = rs.getFloat("foodQuantity");
+                    float carbonQuantity = rs.getFloat("carbonQuantity");
                     // print the results
                     System.out.format("%s, %s, %s\n", foodType, foodQuantity, carbonQuantity);
                 }
@@ -105,14 +79,14 @@ public class AllEmissionsServlet extends HttpServlet {
 		try {
 			PreparedStatement preparedStatement = conn.prepareStatement(sql);
 			preparedStatement.setString(1, username);
-			ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next())
+			ResultSet rs = preparedStatement.executeQuery();
+            while (rs.next())
                 {
                     status = true;
 
                     String transportType = rs.getString("transportType");
-                    float distance = rs.getString("distance");
-                    float carbonQuantity = rs.getDate("carbonQuantity");
+                    float distance = rs.getFloat("distance");
+                    float carbonQuantity = rs.getFloat("carbonQuantity");
                     // print the results
                     System.out.format("%s, %s, %s\n", transportType, distance, carbonQuantity);
                 }
